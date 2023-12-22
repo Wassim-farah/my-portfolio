1ddcc75468c4dd316ee736b67267cc1669b7a34e
wassimfarah <99092856+wassimfarah@users.noreply.github.com>
2023-12-22 15:25:50 +0100

    under-maintenace

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,14 @@
 import Header from './components/header.jsx'
 
 function App() {
+  const maintenace = "We're sorry for the inconvenience. Our website is currently undergoing maintenance. Please check back later."
   return (
     <div className="App container">
       <header className="App-header">
        
-       <Header></Header>
-       
+       <h1 style={{marginTop:"150px",textAlign:"center"}}> {maintenace}</h1>
+{     //  <Header></Header>
+}       
       </header>
     </div>
   );
